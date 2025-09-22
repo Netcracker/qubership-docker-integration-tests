ce475bd379d6986df5bbaed8fb3fbc545c8b7af3
nifa1018 <nikita.fartushnov@netcracker.com>
2025-09-22 16:03:49 +0500

    fix for tags v4

--- scripts/adapter-S3/start_tests.sh
+++ scripts/adapter-S3/start_tests.sh
@@ -5,11 +5,17 @@ ROBOT_ARGS="$@"
 echo "📋 Using robot arguments: $ROBOT_ARGS"
 
 # Execute the script, generate robot and allure results with passed arguments
+# robot --output $ADAPTER_S3_OUT_DIR/output.xml \
+#       --log $ADAPTER_S3_OUT_DIR/log.html \
+#       --report $ADAPTER_S3_OUT_DIR/report.html \
+#       --listener "allure_robotframework;$ADAPTER_S3_OUT_DIR/adapter-S3/allure-results" \
+#       "$@"
+
 robot --output $ADAPTER_S3_OUT_DIR/output.xml \
       --log $ADAPTER_S3_OUT_DIR/log.html \
       --report $ADAPTER_S3_OUT_DIR/report.html \
       --listener "allure_robotframework;$ADAPTER_S3_OUT_DIR/adapter-S3/allure-results" \
-      "$@"
+      -i backup,crud,consul_images -e alerts,consul_images,unauthorized_access,s3_storage /opt/robot/tests
 
 # Capture the exit code from the script
 exit_code=$?
