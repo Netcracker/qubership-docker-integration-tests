d676cc9e567c97463ce8eb50189737ab1d337dd8
nifa1018 <nikita.fartushnov@netcracker.com>
2025-09-24 11:48:05 +0500

    v15

--- scripts/adapter-S3/start_tests.sh
+++ scripts/adapter-S3/start_tests.sh
@@ -29,7 +29,7 @@ robot --output $ADAPTER_S3_OUT_DIR/output.xml \
       --log $ADAPTER_S3_OUT_DIR/log.html \
       --report $ADAPTER_S3_OUT_DIR/report.html \
       --listener "allure_robotframework;$ADAPTER_S3_OUT_DIR/adapter-S3/allure-results" \
-      -i backup -i crud -e consul_images,alerts,unauthorized_access,s3_storage ./tests
+      -i backup -i crud -i consul_images -e consul_images -e alerts -e unauthorized_access -e s3_storage ./tests
 
 # Capture the exit code from the script
 exit_code=$?
