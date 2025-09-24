a304d013e75f0d0ced683962e88d5bc67f731381
nifa1018 <nikita.fartushnov@netcracker.com>
2025-09-24 11:24:43 +0500

    v14

--- scripts/adapter-S3/start_tests.sh
+++ scripts/adapter-S3/start_tests.sh
@@ -18,12 +18,18 @@ echo "📋 Using robot arguments: $@"
 #       --listener "allure_robotframework;$ADAPTER_S3_OUT_DIR/adapter-S3/allure-results" \
 #       -i backup,crud,consul_images -e consul_images,alerts,unauthorized_access,s3_storage ./tests
 
+#Working
+# robot --output $ADAPTER_S3_OUT_DIR/output.xml \
+#       --log $ADAPTER_S3_OUT_DIR/log.html \
+#       --report $ADAPTER_S3_OUT_DIR/report.html \
+#       --listener "allure_robotframework;$ADAPTER_S3_OUT_DIR/adapter-S3/allure-results" \
+#       -i crud -e consul_images,alerts,unauthorized_access,s3_storage ./tests
+
 robot --output $ADAPTER_S3_OUT_DIR/output.xml \
       --log $ADAPTER_S3_OUT_DIR/log.html \
       --report $ADAPTER_S3_OUT_DIR/report.html \
       --listener "allure_robotframework;$ADAPTER_S3_OUT_DIR/adapter-S3/allure-results" \
-      -i crud -e consul_images,alerts,unauthorized_access,s3_storage ./tests
-
+      -i backup -i crud -e consul_images,alerts,unauthorized_access,s3_storage ./tests
 
 # Capture the exit code from the script
 exit_code=$?
