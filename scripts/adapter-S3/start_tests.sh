e041ece5e09f67195ac4ceee28e958a23f6d1929
nifa1018 <nikita.fartushnov@netcracker.com>
2025-09-24 10:56:51 +0500

    v12

--- scripts/adapter-S3/start_tests.sh
+++ scripts/adapter-S3/start_tests.sh
@@ -4,11 +4,17 @@
 echo "📋 Using robot arguments: $@"
 
 # Execute the script, generate robot and allure results with passed arguments
+# robot --output $ADAPTER_S3_OUT_DIR/output.xml \
+#       --log $ADAPTER_S3_OUT_DIR/log.html \
+#       --report $ADAPTER_S3_OUT_DIR/report.html \
+#       --listener "allure_robotframework;$ADAPTER_S3_OUT_DIR/adapter-S3/allure-results" \
+#       "$@"
+
 robot --output $ADAPTER_S3_OUT_DIR/output.xml \
       --log $ADAPTER_S3_OUT_DIR/log.html \
       --report $ADAPTER_S3_OUT_DIR/report.html \
       --listener "allure_robotframework;$ADAPTER_S3_OUT_DIR/adapter-S3/allure-results" \
-      "$@"
+      -i backup,crud,consul_images -e consul_images,alerts,unauthorized_access,s3_storage ./tests
 
 # Capture the exit code from the script
 exit_code=$?
